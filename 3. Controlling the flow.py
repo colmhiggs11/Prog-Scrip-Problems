5fc9e77cb15c5ad4f491b408459f08e0e20690c1
Colm Higgins <G00287906@gmit.ie>
2020-02-13 21:15:00 +0000

    updated code for week 4 after looking through week  notes

--- 3. Controlling the flow.py
+++ 3. Controlling the flow.py
@@ -2,7 +2,17 @@
 # Program to ask user to enter an integer and perform calculations 
 # based on whether that integer is even or odd.
 
-x = int (input ("Please enter a positive integer"))
-while x <= 1:
-    if x % 2 == 0:
-        print(x)
+b = int (input ("Please enter a positive integer"))
+m = 2
+
+# This loop will print all even numbers from integer input to 1
+while b > 1:
+
+    if b % m == 0:
+        b = b/m
+        
+        print (b)
+    elif b % m != 0:
+        b = (b*3)+1
+
+        print(b)
