e3d19e2145dbaa94dafbc3c6a84edb6ab327ea8e
Colm Higgins <G00287906@gmit.ie>
2020-02-25 20:06:12 +0000

    Newton Raphson method-Not working

--- 5. Srqt Function wk6.py
+++ 5. Srqt Function wk6.py
@@ -6,17 +6,48 @@
 ### x2 = x1**0.5
 
 ### print("The square root of {} is {}".format(x1,x2))
-
-b = float(input("Enter the number you want to get Square root approximtaion using Newton Raphson method "))
-
-
-def newton_approx(x,iter = 500):
-    
-    for b in range(iter): # iteration number
-        x = 0.5 * (x + b / x) # update
+#b = float(input("Enter the number you want to get Square root approximtaion using Newton Raphson method "))
+#def newton_approx(x, ):
+   
+   # b = 9
+    #for b in range(1,iter): # iteration number
+   # x = b//2
+   # x = 0.5 * (x + b / x) # update
         #x = (x - (x**2 - N) / (x*2)) # update
 	  # x_(n+1) = 0.5 * (x_n +a / x_n)
-    return x
+ #   return x
+#print(newton_approx(x))
 
+#newton_approx(x)
+#x = int(input("Enter first num"))
+#y = int(input("Enter second num"))
 
-print(newton_approx(b))
+#def smaller_num(x,y):
+#x = int(input("Enter first num"))
+#y = int(input("Enter second num"))
+ #   if x>y:
+  #      number = y
+   # else:
+    #    number = x
+    #return number
+
+#print("Smaller num is " ,(smaller_num(x,y)))
+
+#small = smaller_num(y)
+
+N = int(input("Enter number you are looking for approximate sqrt  "))
+x = N/2
+def f(x,N):
+    fxn = x**2 - N
+    while fxn >= 1:
+        if fxn == 0:
+            print(x)
+        else:
+            fxn = x
+            fxn = x**2 - N
+            fpxn = 2*x
+            sqrt = x - (fxn) / (fpxn)
+            print(sqrt)
+
+f(x, N)
+
