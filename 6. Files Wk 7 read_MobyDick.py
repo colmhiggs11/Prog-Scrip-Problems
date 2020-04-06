8c4d8ea4d01923c134b7aa7fbb870f0fd170079f
Colm Higgins <G00287906@gmit.ie>
2020-04-06 21:37:35 +0100

    adding a function to make code more efficient

--- 6. Files Wk 7 read_MobyDick.py
+++ 6. Files Wk 7 read_MobyDick.py
@@ -2,13 +2,15 @@
 # This program will read in a .txt file and count 
 # the number of chosen characters in that file.
 
-# Inputs take in the required filepath & character wanted to search in file
-x = input("Enter directory of file\n")
-y = input("\nEnter an upper/lowercase letter and\nI'll tell you how many times its used!! \n")
-
 # Opens & reads the file as f, searches for variable y and counts the times used.
 # Using the with statement allows for less code and will 
 # automatically close the file when program has run.
 # https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
-with open(x,'r') as f:
-    print("The letter {} is used".format(y),f.read().count(y),"times")
+def charCounter(x,y):    
+    with open(x,'r') as f:
+        print("The letter {} is used".format(y),f.read().count(y),"times")
+
+# Calling the function for opening,reading the file and counting the selected character
+# Inputs take in the required filepath & character wanted to search in file
+charCounter(input("Enter directory of file\n"),input("\nEnter an upper/lowercase letter and\nI'll tell you how many times its used!! \n"))
+# https://www.quora.com/How-can-take-more-than-one-input-in-one-line-in-python
